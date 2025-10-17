02333a6c4018c71f2e0259f19dd8221f902c9a7d
fabiantfandi-svg <fabiantfandi@gmail.com>
2025-10-17 16:34:45 -0500

    actualizacion

---
+++ libreria2.py
@@ -0,0 +1,11 @@
+import random
+
+print(random.random())
+
+print(random.randint(1, 10))
+
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+print(random.choice(colores))
+
+numeros = [1, 2, 3, 4, 5]
+print(random.shuffle(numeros))
